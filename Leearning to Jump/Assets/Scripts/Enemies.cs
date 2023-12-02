d57fee1e732171bbb2bdf687e41cc43457146ccc
gregelious <90730434+gregelious@users.noreply.github.com>
2023-12-01 19:40:44 -0500

    Update Enemies.cs

--- Leearning to Jump/Assets/Scripts/Enemies.cs
+++ Leearning to Jump/Assets/Scripts/Enemies.cs
@@ -5,52 +5,52 @@ using UnityEngine;
 public class Enemies : MonoBehaviour
 {
     //variables for the game
-    Health damage;
+    Health damage;// damage is an object using Health class
 
-    public GameObject hitPoints;
+    public GameObject hitPoints; //hisPoints is an object
 
     // Vector2 direction;
 
-    public float speed;
-    public bool left = true;
+    public float speed; //variable for movement speed
+    public bool left = true; // sees if enemy is moving left or not
 
     // Start is called before the first frame update
     void Start()
     {
-        damage = hitPoints.GetComponent<Health>();
+        damage = hitPoints.GetComponent<Health>(); //initializes Enemies
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (left)
+        if (left) //if left is true
         {
-            transform.position = new Vector2(transform.position.x - speed, transform.position.y);
+            transform.position = new Vector2(transform.position.x - speed, transform.position.y); // enemy moves left
         }
 
         else
         {
-            transform.position = new Vector2(transform.position.x + speed, transform.position.y);
+            transform.position = new Vector2(transform.position.x + speed, transform.position.y);// moves right
         }
 
 
     }
 
-    void OnTriggerEnter2D(Collider2D coll)
+    void OnTriggerEnter2D(Collider2D coll) // collision function
     {
-        if (coll.tag == "Player")
+        if (coll.tag == "Player") //if it touches player
         {
-            damage.TakeDamage(1);
+            damage.TakeDamage(1); // takes one damage
         }
 
-        if (coll.tag == "Wall" && left == true)
+        if (coll.tag == "Wall" && left == true) //if it touches wall and its moving left
         {
-            left = false;
+            left = false; //go right
         }
 
-        else if (coll.tag == "Wall" && left == false)
+        else if (coll.tag == "Wall" && left == false) //if it touches wall and its moving right
         {
-            left = true;
+            left = true; // go left
         }
     }
 
