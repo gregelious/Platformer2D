92544b87e5a0c1374a46f4b6947713efb7253b6a
gregelious <90730434+gregelious@users.noreply.github.com>
2023-12-01 19:31:59 -0500

    Update Damage.cs

--- Leearning to Jump/Assets/Scripts/Damage.cs
+++ Leearning to Jump/Assets/Scripts/Damage.cs
@@ -5,37 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class Damage : MonoBehaviour
 {
-    public int maxHealth;
-    public int currentHealth;
+    public int maxHealth; //maximum health it can have
+    public int currentHealth; // health right now
 
     // Start is called before the first frame update
     void Start()
     {
-        maxHealth = 2;
-        currentHealth = maxHealth;
+        maxHealth = 2; // sets the lives to 2
+        currentHealth = maxHealth; //makes the health right now the maximum it can be
     }
 
-    // Update is called once per frame
-    void Update()
+    void TakeDamage(int amount) //function to lose health
     {
+        currentHealth -= amount; //health goes down by 'amount'
 
-    }
-
-    void TakeDamage(int amount)
-    {
-        currentHealth -= amount;
-
-        if (currentHealth <= 0)
+        if (currentHealth <= 0) //if health right now less than or equal to 0
         {
-            SceneManager.LoadScene("GameOver");
+            SceneManager.LoadScene("GameOver"); //Loads end scene
         }
     }
 
-    void OnTriggerEnter2D(Collider2D coll)
+    void OnTriggerEnter2D(Collider2D coll) // collider function
     {
-        if (coll.tag == "Player")
+        if (coll.tag == "Player") //if it touches player
         {
-            TakeDamage(1);
+            TakeDamage(1); //loses one health
         }
     }
 }
